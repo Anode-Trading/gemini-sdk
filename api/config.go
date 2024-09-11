781fe402a5aa80bd27cc28a32c00c234789ebb98
kmurari <m007urray@gmail.com>
2024-09-11 18:45:51 +0530

    symbol and detail : added apis for allsymbolls and symboldetails and refractoring

--- api/config.go
+++ api/config.go
@@ -1,7 +1,10 @@
 package api
 
 const (
-	baseUrl       = "wss://api.gemini.com/"
-	v1            = "v1/"
-	orderBookPath = "marketData/"
+	wsBaseUrl      = "wss://api.gemini.com/"
+	httpBaseUrl    = "https://api.gemini.com/"
+	v1             = "v1/"
+	orderBookPath  = "marketData/"
+	symbols        = "symbols"
+	symbolsDetails = "symbols/details/%s"
 )
---
+++ api/gemini_http.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func GetSecurities() ([]string, error) {
+	var securities []string
+
+	url := fmt.Sprintf("%s%s%s", httpBaseUrl, v1, symbols)
+
+	symbolResponse, err := http.Get(url)
+	if err != nil || symbolResponse == nil || symbolResponse.Body == nil || symbolResponse.StatusCode != http.StatusOK {
+		return securities, fmt.Errorf("cannot get symbol details: %s", err)
+	}
+	defer symbolResponse.Body.Close()
+	symbolResponseBody, err := io.ReadAll(symbolResponse.Body)
+	if err != nil {
+		return securities, fmt.Errorf("err reading symbol details: %s", err)
+	}
+
+	err = json.Unmarshal(symbolResponseBody, &securities)
+	if err != nil {
+		return securities, fmt.Errorf("err creating symbol details: %s", err)
+	}
+	return securities, nil
+}
+func GetSecurityInfo(pair string) (SymbolDetails, error) {
+	var symbolDetails SymbolDetails
+
+	url := fmt.Sprintf("%s%s%s", httpBaseUrl, v1, fmt.Sprintf(symbolsDetails, pair))
+	symbolDetailsResponse, err := http.Get(url)
+	if err != nil || symbolDetailsResponse == nil || symbolDetailsResponse.Body == nil || symbolDetailsResponse.StatusCode != http.StatusOK {
+		return symbolDetails, fmt.Errorf("cannot get symbol details: %s", err)
+	}
+	defer symbolDetailsResponse.Body.Close()
+
+	symbolDetailResponseBody, err := io.ReadAll(symbolDetailsResponse.Body)
+	if err != nil {
+		return symbolDetails, fmt.Errorf("err reading symbol details: %s", err)
+	}
+
+	err = json.Unmarshal(symbolDetailResponseBody, &symbolDetails)
+	if err != nil {
+		return symbolDetails, fmt.Errorf("err creating symbol details: %s", err)
+	}
+	return symbolDetails, nil
+}
